2eaefb81f5cdca35b93f8e9b2764d93c0699f7f0
sharpTshark <71552553+sharpTshark@users.noreply.github.com>
2020-10-17 21:03:48 +0200

    text index

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
         <!-- <h1>PARTYGAMES</h1> -->
         <h4>Here you can have some real fun with your online friends by playing some little tournaments.</h4>
         <h4>this web app allows you to organise easily your own tournament.</h4>
-        <h4>It knows every game so there is unlimited fun and posibility so you never done playing tournaments.</h4>
-        <h4>if it does'nt you can simply add it in the settings.</h4>
+        <h4>It knows every game so there is unlimited fun, posibility and never done playing tournaments.</h4>
+        <h4>if it doesn't you can simply add it in the settings.</h4>
     </div>
     <div class="top-tile-text">
         <h4>Get started below by joining or rather host a room.</h4>
