70e8e5bf12c10cae2e43835c8a195320d23eea02
sharpTshark <71552553+sharpTshark@users.noreply.github.com>
2020-10-18 12:03:38 +0200

    update title

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="style/styles.css">
     <link rel="icon" href="img/icon.png" type="image/gif" sizes="16x16">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-    <title>PARTY GAMEZZZ</title>
+    <title>PARTYGAMES ONLINE</title>
     
     
 
